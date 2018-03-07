ba68c542e1c1912cd58acb0a0bb8cd0a822c0ce3
joshuasecor <joshuasecor@gmail.com>
2018-03-06 22:24:28 -0800

    setup initial Angular app with nav and reports components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReportsComponent } from './reports/reports.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/reports', pathMatch: 'full' },
+  { path: 'reports', component: ReportsComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-
+import { AppService } from './app.service';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReportsComponent } from './reports/reports.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ReportsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AppService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
